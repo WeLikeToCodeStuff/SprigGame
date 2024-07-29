4a8c697c88d93d0464879319bdb7a13edb4c2db0
Bonzai-Dev <thelamps50@gmail.com>
2024-07-28 20:32:45 -0400

    Added tetrominoe types

--- index.js
+++ index.js
@@ -3,50 +3,107 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Tetris
-@author: BonzaiDev, Adrian T
+@author: BonzaiDev, Adrian T.
 @tags: []
 @addedOn: 2024-07-23
 */
 
 // Game settings
-const blockSpawnPosition = {x: 5, y: 2};
+let blockDropSpeed = 1; // In seconds
+const blockSpawnPosition = {x: 6, y: 0};
 
 const gameMap = map`
-............
-............
-............
-............
-............
-............
-............
-............
-............
-............
-............
-............`
+.............
+.............
+.............
+.............
+.............
+.............
+.............
+.............
+.............
+.............
+.............
+.............
+.............`;
+setMap(gameMap)
+
+// Classes
+const TetrominoTypes = {
+  // Names based on wikipedia: https://en.wikipedia.org/wiki/Tetromino
+  straightTetromino: "straightTetromino",
+  squareTetromino: "squareTetromino",
+  tTetromino: "tTetromino",
+  lTetromino: "lTetromino",
+  skewTetromino: "skewTetromino",
+};
+
+class Tetromino {
+  constructor(bitmapKey, type) {
+    this.bitmapKey = bitmapKey;
+    this.type = type;  
+  };  
+};
+
+// Functions
+function InitializeTetromino(tetromino) {
+  switch (tetromino.type) {
+    case TetrominoTypes.straightTetromino:
+      for (let ySpawn = 0; ySpawn < 4; ySpawn++)
+        addSprite(blockSpawnPosition.x, blockSpawnPosition.y + ySpawn, tetromino.bitmapKey);
+    
+    case TetrominoTypes.squareTetromino:
+      for (let xSpawn = 0; xSpawn < 2; xSpawn++) {
+        for (let ySpawn = 0; ySpawn < 2; ySpawn++) {
+          addSprite(blockSpawnPosition.x + xSpawn, blockSpawnPosition.y + ySpawn, tetromino.bitmapKey);
+        };
+      };
+
+    case TetrominoTypes.tTetromino:
+      addSprite(blockSpawnPosition.x + 1, blockSpawnPosition.y + 1, tetromino.bitmapKey);
+      for (let xSpawn = 0; xSpawn < 3; xSpawn++) 
+        addSprite(blockSpawnPosition.x + xSpawn, blockSpawnPosition.y, tetromino.bitmapKey);
+  
+    case TetrominoTypes.lTetromino:
+      for (let ySpawn = 0; ySpawn < 3; ySpawn++)
+        addSprite(blockSpawnPosition.x, blockSpawnPosition.y + ySpawn, tetromino.bitmapKey);
+      addSprite(blockSpawnPosition.x + 1, blockSpawnPosition.y + 2, tetromino.bitmapKey);
+  
+    case TetrominoTypes.skewTetromino:
+      for (let xSpawn = 0; xSpawn < 2; xSpawn++) {
+        addSprite(blockSpawnPosition.x + xSpawn, blockSpawnPosition.y, tetromino.bitmapKey);
+      };
+
+      for (let xSpawn = 0; xSpawn < 2; xSpawn++) {
+        addSprite(blockSpawnPosition.x + xSpawn - 1, blockSpawnPosition.y + 1, tetromino.bitmapKey);
+      };
+  }; 
+}
 
 // Sprites
-const player = "p";
+const newTetromino = new Tetromino("b", TetrominoTypes.skewTetromino);
 
 setLegend(
-  [ player, bitmap`
-................
-................
-................
-.......0........
-.....00.000.....
-....0.....00....
-....0.0.0..0....
-....0......0....
-....0......0....
-....00....0.....
-......00000.....
-......0...0.....
-....000...000...
-................
-................
-................`]
+  [ newTetromino.bitmapKey, bitmap`
+111111111111111L
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+0LLLLLLLLLLLLLL1
+L111111111111111`]
 );
+
+InitializeTetromino(newTetromino)
 
 // Inputs
 onInput("w", () => {
@@ -66,7 +123,8 @@ onInput("d", () => {
 });
 
 // Game logic
-setMap(gameMap)
-
-addSprite(blockSpawnPosition.x, blockSpawnPosition.y, player);
-
+setInterval(function() {
+  getAll(newTetromino.bitmapKey).forEach(function(block) {
+    block.y += 1;
+  });
+}, blockDropSpeed * 1000); 
